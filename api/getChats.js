a63e0a34fc5be847b94ea3747014f8de2a065f7d
Alira Coffman <aliracoffman@Aliras-Laptop.local>
2024-01-08 18:31:36 -0600

    remove const error

--- api/getChats.js
+++ api/getChats.js
@@ -2,16 +2,8 @@ import { db } from "@/firebaseconfig";
 import { getDocs, collection, query } from "firebase/firestore"
 export async function getChats(filters = {}) {
     let q = query(collection(db, "chats"));
-    const results = null;
-    const error = null;
-    try {
-        results = await getDocs(q);
-    } catch (e) {
-        error = e;
-    }
-
-
-    console.log('results', results.docs)
+    const results = await getDocs(q);
+    console.log('results', results)
     return results.docs.map(doc => {
         return {
             id: doc.id,
